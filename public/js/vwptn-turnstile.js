72ee3e2eb92bf0ad0d01bb5823cb87f2f22c2fad
orin-sightfactory <63678490+orin-sightfactory@users.noreply.github.com>
2022-11-22 14:19:34 -0400

    Update vwptn-turnstile.js

--- public/js/vwptn-turnstile.js
+++ public/js/vwptn-turnstile.js
@@ -5,26 +5,32 @@ jQuery(document).ready(function(){
 jQuery(document).on( 'nfFormReady', function() {	
 		jQuery('.vwpturnstile-wrap .nf-field-element').append('<div class="cf-turnstile"></div>');	
 		
-		
+		//fetch site key and status
 		jQuery.ajax({
+			dataType: "json",
 			url: vwpscripts.siteUrl+'/wp-admin/admin-ajax.php',
 			type: 'post',
-			data: { action: 'vwptn_turnstile_data_fetch'},
-			success: function(vwp_turnstile_site_key) {
+			data: { action: 'vwptn_turnstile_data_fetch','nonce':vwpscripts.nonce},
+			success: function(vwptn_settings) {				
+				var vwptn_status = vwptn_settings.vwptnstatus;
+				//console.log(vwptn_status);
+				var vwptn_turnstile_site_key = vwptn_settings.sitekey;
 				
-				turnstile.render('.cf-turnstile', {
-					sitekey: vwp_turnstile_site_key,
-					callback: function(token) {
-						jQuery('.vwpturnstile-wrap input').val(token);
-						
-						setTimeout(function() {
-							jQuery('.vwpturnstile-wrap input').trigger('change');
+				if(vwptn_status == 1) {
+					turnstile.render('.cf-turnstile', {
+						sitekey: vwptn_turnstile_site_key,
+						callback: function(token) {
+							jQuery('.vwpturnstile-wrap input').val(token);
 							
-						}, 500);
-						
-						
-					},
-				});	
+							setTimeout(function() {
+								jQuery('.vwpturnstile-wrap input').trigger('change');
+								
+							}, 500);
+							
+							
+						},
+					});	
+				}
 				
 				
 			}
