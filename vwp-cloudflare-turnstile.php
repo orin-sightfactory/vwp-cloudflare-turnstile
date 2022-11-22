71655a8a112859c658b05e7e6784ebaa9a869e12
orin-sightfactory <63678490+orin-sightfactory@users.noreply.github.com>
2022-11-22 10:05:53 -0400

    Update vwp-cloudflare-turnstile.php

--- vwp-cloudflare-turnstile.php
+++ vwp-cloudflare-turnstile.php
@@ -8,10 +8,10 @@
  
 /*
  
-Plugin Name: VisualWP - Cloudflare Turnstile 
+Plugin Name: VisualWP Cloudflare Turnstile 
 Plugin URI: https://sightfactory.com/wordpress-plugins/turnstile 
-Description: Increase security and protect against bots, spammers and hackers. Add Cloudflare Turnstile to WordPress and Ninja Forms.
-Version: 1.0.1
+Description: Increase security and protect against bots, spammers and hackers. Adds Cloudflare Turnstile to WordPress and Ninja Forms. Cloudflare Turnstile is a friendly, free CAPTCHA replacement that delivers a frustration-free experience to website visitors.
+Version: 1.0.2
 Author: Sightfactory 
 Author URI: https://sightfactory.com/wordpress-plugins/
 Requires at least: 5.9
@@ -25,35 +25,35 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly.
 }
 
-function vwp_turnstile_add_settings_link( $links ) {
-    $settings_link = '<a href="options-general.php?page=vwp-turnstile">' . __( 'Settings' ) . '</a>';
+function vwptn_turnstile_add_settings_link( $links ) {
+    $settings_link = '<a href="options-general.php?page=vwptn-turnstile">' . __( 'Settings' ) . '</a>';
     array_push( $links, $settings_link );
   	return $links;
 }
 $plugin = plugin_basename( __FILE__ );
-add_filter( "plugin_action_links_$plugin", 'vwp_turnstile_add_settings_link' );
+add_filter( "plugin_action_links_$plugin", 'vwptn_turnstile_add_settings_link' );
 
 
-function vwp_init_turnstile_scripts() {
+function vwptn_init_turnstile_scripts() {
+	
 	if ( $GLOBALS['pagenow'] === 'wp-login.php' ) {
-		wp_enqueue_script( 'vwp-turnstilejs', 'https://challenges.cloudflare.com/turnstile/v0/api.js' , '1.0', true );
-		//wp_enqueue_script( 'vwp-turnstilejs-render', 'https://challenges.cloudflare.com/turnstile/v0/api.js?onload=onloadTurnstileCallback' , '1.0', true );
+		wp_enqueue_script( 'vwptn-turnstilejs', 'https://challenges.cloudflare.com/turnstile/v0/api.js' , '1.0', true );
+		//wp_enqueue_script( 'vwptn-turnstilejs-render', 'https://challenges.cloudflare.com/turnstile/v0/api.js?onload=onloadTurnstileCallback' , '1.0', true );
 
 	}
 	else {
-		wp_enqueue_script( 'vwp-turnstilejs-render', 'https://challenges.cloudflare.com/turnstile/v0/api.js' , '1.0', true );
+		wp_enqueue_script( 'vwptn-turnstilejs-render', 'https://challenges.cloudflare.com/turnstile/v0/api.js' , '1.0', true );
 		if(!is_admin()) {
-			wp_enqueue_script('vwp-turnstile-js', plugins_url('/public/js/vwp-turnstile.js' , __FILE__) , array('jquery','vwp-turnstilejs-render'));
-			wp_localize_script('vwp-turnstile-js', 'vwpscripts', array(
+			wp_enqueue_script('vwptn-turnstile-js', plugins_url('/public/js/vwptn-turnstile.js' , __FILE__) , array('jquery','vwptn-turnstilejs-render'),'1.2');
+			wp_localize_script('vwptn-turnstile-js', 'vwpscripts', array(
 				'siteUrl' => get_site_url(),
 			));
-			wp_enqueue_style('vwp-turnstyle-css', plugins_url('/public/css/vwp-turnstile.css' , __FILE__), array());
+			wp_enqueue_style('vwptn-turnstyle-css', plugins_url('/public/css/vwp-turnstile.css' , __FILE__), array(),'1.2');
 		}
 		
 		
 		if(is_admin()) {
-			wp_enqueue_style('vwp-turnstyle-admin-css', plugins_url('/admin/css/vwp.css' , __FILE__), array());
-			wp_enqueue_style('vwp-gf-sen','https://fonts.googleapis.com/css2?family=Sen:wght@400;700&display=swap');
+			wp_enqueue_style('vwptn-turnstyle-admin-css', plugins_url('/admin/css/vwp.css' , __FILE__), array(),'1.2');
 		}
 		
 	}
@@ -61,10 +61,10 @@ function vwp_init_turnstile_scripts() {
 
 }
 
-function vwp_init_turnstile_verification(WP_User $user) {
+function vwptn_init_turnstile_verification(WP_User $user) {
 	
 	$data = sanitize_text_field($_POST['cf-turnstile-response']);
-	$response = vwp_get_turnstile_response($data);	
+	$response = vwptn_get_turnstile_response($data);	
 	if($response != 1) {
 		$user = new WP_Error( 'authentication_failed',esc_html($response));		  
 	}
@@ -72,21 +72,21 @@ function vwp_init_turnstile_verification(WP_User $user) {
 
 }
 
-function vwp_init_turnstile_verification_register($errors, $sanitized_user_login, $user_email ) {
+function vwptn_init_turnstile_verification_register($errors, $sanitized_user_login, $user_email ) {
 	
 	$data = sanitize_text_field($_POST['cf-turnstile-response']);
-	$response = vwp_get_turnstile_response($data);	
+	$response = vwptn_get_turnstile_response($data);	
 	if($response != 1) {
-		    $errors->add( 'vwp_turnstile_error', __( esc_html($response) ) );  
+		    $errors->add( 'vwptn_turnstile_error', __( esc_html($response) ) );  
 	}
 	return $errors;
 
 }
 
-function vwp_init_turnstile_verification_comment($commentdata) {
+function vwptn_init_turnstile_verification_comment($commentdata) {
 	
 	$data = sanitize_text_field($_POST['cf-turnstile-response']);
-	$response = vwp_get_turnstile_response($data);
+	$response = vwptn_get_turnstile_response($data);
 	
 	if($response != 1) {
 		
@@ -98,53 +98,53 @@ function vwp_init_turnstile_verification_comment($commentdata) {
 
 }
 
-add_action( 'login_enqueue_scripts', 'vwp_init_turnstile_scripts' );
+add_action( 'login_enqueue_scripts', 'vwptn_init_turnstile_scripts' );
 
 
-add_action( 'init', 'vwp_init_turnstile_scripts' );
+add_action( 'init', 'vwptn_init_turnstile_scripts' );
 
-@$vwp_turnstile_key_check = get_option('vwp_turnstile_site_key');
+@$vwptn_turnstile_key_check = get_option('vwptn_turnstile_site_key');
 
-if(strlen($vwp_turnstile_key_check) > 0) {
+if(strlen($vwptn_turnstile_key_check) > 0) {
 /*Login*/
-add_action( 'login_form', 'vwp_init_turnstile_widget' );
-add_action('wp_authenticate_user','vwp_init_turnstile_verification',10, 1);
+add_action( 'login_form', 'vwptn_init_turnstile_widget' );
+add_action('wp_authenticate_user','vwptn_init_turnstile_verification',10, 1);
 
 /*Regsistration*/
-add_action('register_form','vwp_init_turnstile_widget');
-add_action('registration_errors', 'vwp_init_turnstile_verification_register', 10, 3);
+add_action('register_form','vwptn_init_turnstile_widget');
+add_action('registration_errors', 'vwptn_init_turnstile_verification_register', 10, 3);
 
 /*Lost Password*/
-add_action('lostpassword_form','vwp_init_turnstile_widget');
-add_action('lostpassword_post','vwp_init_turnstile_verification', 10, 1);
+add_action('lostpassword_form','vwptn_init_turnstile_widget');
+add_action('lostpassword_post','vwptn_init_turnstile_verification', 10, 1);
 
 /*WordPress Comments*/
-add_action("comment_form_logged_in_after", "vwp_init_turnstile_widget");
-add_action("comment_form_after_fields", "vwp_init_turnstile_widget");
-add_action('preprocess_comment','vwp_init_turnstile_verification_comment', 10, 1);
+add_action("comment_form_logged_in_after", "vwptn_init_turnstile_widget");
+add_action("comment_form_after_fields", "vwptn_init_turnstile_widget");
+add_action('preprocess_comment','vwptn_init_turnstile_verification_comment', 10, 1);
 
 //add_action('comment_form_submit_button','cfturnstile_field_comment', 100, 2);
 
 /*Ninja Forms*/
-add_action('lostpassword_form','vwp_init_turnstile_widget');
-add_action('lostpassword_post','vwp_init_turnstile_verification', 10, 1);
+add_action('lostpassword_form','vwptn_init_turnstile_widget');
+add_action('lostpassword_post','vwptn_init_turnstile_verification', 10, 1);
 
 /*Woocommerce*/
 //TODO
 }
 /**/
-function vwp_init_turnstile_widget() { 
-	$vwp_turnstile_site_key_value = get_option('vwp_turnstile_site_key');
-	echo sprintf('<div class="cf-turnstile" data-sitekey="%s"></div><style>.cf-turnstile iframe { max-width:270px !important}</style>',esc_html($vwp_turnstile_site_key_value));
+function vwptn_init_turnstile_widget() { 
+	$vwptn_turnstile_site_key_value = get_option('vwptn_turnstile_site_key');
+	echo sprintf('<div class="cf-turnstile" data-sitekey="%s"></div><style>.cf-turnstile iframe { max-width:270px !important}</style>',esc_html($vwptn_turnstile_site_key_value));
 
 }
 
-function vwp_get_turnstile_response($data) {
-	$vwp_turnstile_secret_key_value = get_option('vwp_turnstile_secret_key');	
+function vwptn_get_turnstile_response($data) {
+	$vwptn_turnstile_secret_key_value = get_option('vwptn_turnstile_secret_key');	
 	$request = NULL; 
 	$headers = array(
 				'body' => [
-					'secret' => esc_html($vwp_turnstile_secret_key_value),
+					'secret' => esc_html($vwptn_turnstile_secret_key_value),
 					'response' => $data
 				]
 			);
@@ -173,7 +173,7 @@ function vwp_get_turnstile_response($data) {
 			
 			$errors = '';
 			foreach($response->{'error-codes'} as $code) {
-				$errors .= vwp_get_turnstile_errors($code);
+				$errors .= vwptn_get_turnstile_errors($code);
 			}
 			return esc_html($errors);
 		
@@ -188,7 +188,7 @@ function vwp_get_turnstile_response($data) {
 	
 }
 
-function vwp_get_turnstile_errors($error_code) {
+function vwptn_get_turnstile_errors($error_code) {
 	switch ($error_code) {
 		case 'missing-input-secret':
 			return esc_html('An unexpected error occurred. The secret parameter was not passed.');
@@ -237,8 +237,8 @@ if ( in_array( 'ninja-forms/ninja-forms.php', apply_filters( 'active_plugins', g
 	}
 
 	// Set value for vwpturnstile field
-	add_filter( 'ninja_forms_render_default_value', 'nf_default_value_vwpturnstile' , 10 , 3);
-	function nf_default_value_vwpturnstile( $default_value, $field_type, $field_settings ) {
+	add_filter( 'ninja_forms_render_default_value', 'vwptn_default_value_vwpturnstile' , 10 , 3);
+	function vwptn_default_value_vwpturnstile( $default_value, $field_type, $field_settings ) {
 		
 		if ( 'vwpturnstile' == $field_type && in_array( 'vwpturnstile', $field_settings ) ) {
 			$default_value = '';
@@ -247,15 +247,15 @@ if ( in_array( 'ninja-forms/ninja-forms.php', apply_filters( 'active_plugins', g
 	}
 
 
-	add_filter( 'ninja_forms_submit_data', 'my_ninja_forms_submit_data' );
+	add_filter( 'ninja_forms_submit_data', 'vwptn_ninja_forms_submit_data' );
 
-	function my_ninja_forms_submit_data( $form_data ) {
+	function vwptn_ninja_forms_submit_data( $form_data ) {
  
-	  foreach( $form_data[ 'fields' ] as $field ) { // Field settigns, including the field key and value.
+	  foreach( $form_data[ 'fields' ] as $field ) { // Field settings, including the field key and value.
 	   
 	  if(stristr($field['key'],'turnstile')) {
 		  
-	   $response = esc_html(vwp_get_turnstile_response($field['value']));	
+	   $response = esc_html(vwptn_get_turnstile_response($field['value']));	
 	   
 		   if($response != 1) {
 			   
@@ -292,8 +292,8 @@ if ( in_array( 'ninja-forms/ninja-forms.php', apply_filters( 'active_plugins', g
 
 }
 
-if( ! function_exists( 'vwp_current_user_has_role' ) ){
-    function vwp_current_user_has_role( $role ) {
+if( ! function_exists( 'vwptn_current_user_has_role' ) ){
+    function vwptn_current_user_has_role( $role ) {
 
         $user = get_userdata( get_current_user_id() );
         if( ! $user || ! $user->roles ){
@@ -308,13 +308,13 @@ if( ! function_exists( 'vwp_current_user_has_role' ) ){
     }
 }
 
-add_action( 'admin_menu', 'vwp_turnstile_menu' );
+add_action( 'admin_menu', 'vwptn_turnstile_menu' );
 
-function vwp_turnstile_menu() {
-	add_options_page( 'Turnstile', 'Turnstile', 'manage_options', 'vwp-turnstile', 'vwp_turnstile_menu_options' );
+function vwptn_turnstile_menu() {
+	add_options_page( 'Turnstile', 'Turnstile', 'manage_options', 'vwptn-turnstile', 'vwptn_turnstile_menu_options' );
 }
 
-function vwp_turnstile_menu_options() {
+function vwptn_turnstile_menu_options() {
 	$update_notice = '';
 	$plugin_title = get_admin_page_title();
     $plugin_page = "Turnstile Options";
@@ -327,30 +327,30 @@ function vwp_turnstile_menu_options() {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
 	else {
-		if (isset($_POST['vwp_turnstile_site_key']) && vwp_current_user_has_role( 'administrator' ) )  {	
-			check_admin_referer( 'vwp_option_page_action' );
-			update_option('vwp_turnstile_site_key', sanitize_text_field($_POST['vwp_turnstile_site_key']));
-			update_option('vwp_turnstile_secret_key', sanitize_text_field($_POST['vwp_turnstile_secret_key']));
+		if (isset($_POST['vwptn_turnstile_site_key']) && vwptn_current_user_has_role( 'administrator' ) )  {	
+			check_admin_referer( 'vwptn_option_page_action' );
+			update_option('vwptn_turnstile_site_key', sanitize_text_field($_POST['vwptn_turnstile_site_key']));
+			update_option('vwptn_turnstile_secret_key', sanitize_text_field($_POST['vwptn_turnstile_secret_key']));
 			$update_notice = 'Settings Updated';
 		} 
 		
 	
-		$vwp_turnstile_site_key_value = get_option('vwp_turnstile_site_key');
-		$vwp_turnstile_secret_key_value = get_option('vwp_turnstile_secret_key');
+		$vwptn_turnstile_site_key_value = get_option('vwptn_turnstile_site_key');
+		$vwptn_turnstile_secret_key_value = get_option('vwptn_turnstile_secret_key');
 	?>
 	<div class="wrap">
 	
-	<div class="vwp-plugin-header">
+	<div class="vwptn-plugin-header">
 	<a href="https://www.sightfactory.com/wordpress-plugins/visual-wp" target="_blank">
 	<img align="center" style="width:25px;margin-top:-5px" src="<?php echo plugins_url('/admin/images/vwp-logo.png' , __FILE__)?>"/> <span class="plugin-creator">VisualWP</span></a><?php esc_html_e($plugin_title) ?>
 	
 	
 	<img align="center" style="float:right;width:55px;margin-top:-15px" src="<?php echo plugins_url('/admin/images/vwp-turnstile-logo.gif' , __FILE__)?>"/>
 	</div>
-	<div class="vwp-plugin-body">
+	<div class="vwptn-plugin-body">
 		
 		<?php 
-		echo sprintf("<span class='vwp-notice'>%s</span>",esc_html($update_notice));
+		echo sprintf("<span class='vwptn-notice'>%s</span>",esc_html($update_notice));
 		$update_notice = '';
 		?>
 		
@@ -358,19 +358,51 @@ function vwp_turnstile_menu_options() {
 		<div>
 		<label for="awesome_text"><p>Turnstile Site Key</p></label>
 		
-		<input type="password" name="vwp_turnstile_site_key" id="turnstile_site_key" size="40" value="<?php echo esc_html(@$vwp_turnstile_site_key_value); ?>">
+		<input type="password" name="vwptn_turnstile_site_key" id="turnstile_site_key" size="40" value="<?php echo esc_html(@$vwptn_turnstile_site_key_value); ?>">
 		</div>
 		<div>
 		 <label for="awesome_text"><p>Turnstile Secret Key</p></label>
-		<input type="password" name="vwp_turnstile_secret_key" id="turnstile_secret_key" size="40" value="<?php echo esc_html(@$vwp_turnstile_secret_key_value); ?>">
+		<input type="password" name="vwptn_turnstile_secret_key" id="turnstile_secret_key" size="40" value="<?php echo esc_html(@$vwptn_turnstile_secret_key_value); ?>">
 		</div>
-		<?php wp_nonce_field( 'vwp_option_page_action' ); ?>
+		<?php wp_nonce_field( 'vwptn_option_page_action' ); ?>
 		<br>
 		<input type="submit" value="Save Changes" class="button button-primary button-large">
 		
 		</form>
 		<p>Don't have a Cloudflare Turnstile key? <a href="https://www.cloudflare.com/lp/turnstile/" target="_blank">Create one here</a></p>.
-		<!--<p><a href="https://www.sightfactory.com/wordpress-plugins/vwp-turnstile" target="_blank">View Documentation</a></p>-->
+		<p><a href="https://www.cloudflare.com/website-terms/" target="_blank">Cloudflare Website and Online Services Terms of Use</a></p>
+		<p><a href="https://www.cloudflare.com/privacypolicy/" target="_blank">Cloudflare Privacy Policy</a></p>
+		<p><a href="https://www.sightfactory.com/wordpress-plugins/vwptn-turnstile" target="_blank">Need help? View Documentation</a></p>
+		
+		<?php
+		if(isset($vwptn_turnstile_site_key_value) && strlen($vwptn_turnstile_site_key_value) > 0 ) {
+			?>
+		
+		<form method="POST">
+		<?php vwptn_init_turnstile_widget(); ?>
+		
+		<input type="submit" name="vwptn_turnstile_site_test_button" value="Test Website Configuration" class="button button-primary button-large">
+		<?php wp_nonce_field( 'vwptn_option_page_action' ); ?>
+		<?php 
+		if($_POST && isset($_POST['vwptn_turnstile_site_test_button'])) {
+			check_admin_referer( 'vwptn_option_page_action' );
+			$vwptn_test_data = sanitize_text_field($_POST['cf-turnstile-response']);
+			$vwptn_response = vwptn_get_turnstile_response($vwptn_test_data);
+			if($vwptn_response == 1) {
+				$vwptn_success_text = 'Website was successfully configured.';
+				echo sprintf('<p class="vwptn-notice"><b>%s</b></p>',esc_html($vwptn_success_text));
+			}
+			else {
+				echo sprintf('<p class="vwptn-notice"><b>%s</b></p>',esc_html($vwptn_response));
+				
+			}
+		}
+		?>
+		</form>
+		<?php
+		} // end configuration check
+		?>
+		
 		
 	</div>
 	</div>
@@ -382,9 +414,9 @@ function vwp_turnstile_menu_options() {
 }
 
 
-function vwp_turnstile_defer_scripts( $tag, $handle, $src ) {
+function vwptn_turnstile_defer_scripts( $tag, $handle, $src ) {
   $defer = array( 
-    'vwp-turnstilejs'
+    'vwptn-turnstilejs'
   );
 
   if ( in_array( $handle, $defer ) ) {
@@ -394,13 +426,13 @@ function vwp_turnstile_defer_scripts( $tag, $handle, $src ) {
     return $tag;
 } 
 
-add_filter( 'script_loader_tag', 'vwp_turnstile_defer_scripts', 10, 3 );
+add_filter( 'script_loader_tag', 'vwptn_turnstile_defer_scripts', 10, 3 );
 
 // Provides a way to get the site key for forms that are created dynamically
-add_action('wp_ajax_vwp_turnstile_data_fetch' , 'vwp_data_fetch');
-add_action('wp_ajax_nopriv_vwp_turnstile_data_fetch','vwp_data_fetch');
-function vwp_data_fetch(){
-	esc_html_e(get_option('vwp_turnstile_site_key'));
+add_action('wp_ajax_vwptn_turnstile_data_fetch' , 'vwptn_data_fetch');
+add_action('wp_ajax_nopriv_vwptn_turnstile_data_fetch','vwptn_data_fetch');
+function vwptn_data_fetch(){
+	esc_html_e(get_option('vwptn_turnstile_site_key'));
 	wp_die();
 }
 
